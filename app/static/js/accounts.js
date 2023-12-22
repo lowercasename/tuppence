004e5c00ce7cd249354dfe6b42d2e4a1999cf0e9
Raphael Kabo <mail@raphaelkabo.com>
2023-12-22 21:20:57 +0000

    Fix mobile scroll

--- app/static/js/accounts.js
+++ app/static/js/accounts.js
@@ -1,6 +1,9 @@
 var el = document.querySelectorAll('tbody')[0];
 var sortable = Sortable.create(el, {
     ghostClass: "sortable-ghost",
+    delay: 500,
+    delayOnTouchOnly: true,
+    animation: 150,
     // Disable sorting on the `end` event
     onEnd: function () {
         this.option("disabled", true);
