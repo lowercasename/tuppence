52558fe4ef43102525eebe75390d064a57163476
Raphael Kabo <mail@raphaelkabo.com>
2023-12-22 13:42:49 +0000

    Update deploy script to install packages

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ LOG_FILE=deploy.log
 
 echo "Deploying at $(date)" >> $LOG_FILE 
 git pull 2>> $LOG_FILE
+pip install -r requirements.txt 2>> $LOG_FILE
 systemctl restart tuppence.service 2>> $LOG_FILE
 echo "Deploy complete at $(date)" >> $LOG_FILE
