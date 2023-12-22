4ae78468552713510ec5df4b817d88d8e2e8736f
Raphael Kabo <mail@raphaelkabo.com>
2023-12-22 21:04:30 +0000

    Fix sortable on mobile

--- app/static/js/pots.js
+++ app/static/js/pots.js
@@ -1,10 +1,13 @@
 var el = document.querySelectorAll('.pots')[0];
 var sortable = Sortable.create(el, {
     ghostClass: "sortable-ghost",
+    delay: 500,
+    delayOnTouchOnly: true,
+    animation: 150,
     // Disable sorting on the `end` event
     onEnd: function () {
         this.option("disabled", true);
-    }
+    },
 });
 
 // Re-enable sorting on the `htmx:afterSwap` event
