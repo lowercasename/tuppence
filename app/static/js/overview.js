3dfe0e5e0f8f932c4773d373541cf92b91485abf
Raphael Kabo <mail@raphaelkabo.com>
2023-12-22 00:06:05 +0000

    Use hx-boost for navigation

---
+++ app/static/js/overview.js
@@ -0,0 +1,73 @@
+// These have to be var because HTMX re-evaluates the script tag
+// when using hx-boost
+var balanceCtx = document.getElementById('balanceChart');
+var categoriesCtx = document.getElementById('categoriesChart');
+
+new Chart(balanceCtx, {
+    type: 'line',
+    data: {
+        labels: balanceData[0].balances.map((_, i) => i),
+        datasets: balanceData.map(a => ({
+            label: a.name,
+            data: a.balances.map(b => b / 100),
+        }))
+    },
+    options: {
+        scales: {
+            y: {
+                ticks: {
+                    callback: function (value, index, values) {
+                        return value.toLocaleString("en-GB", { style: "currency", currency: "GBP" });
+                    }
+                }
+            },
+            x: {
+                ticks: {
+                    callback: function (value, index, values) {
+                        return value + 1;
+                    }
+                }
+            }
+        },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        return context.dataset.label + ': ' + context.parsed.y.toLocaleString("en-GB", { style: "currency", currency: "GBP" });
+                    }
+                }
+            }
+        }
+    }
+});
+
+new Chart(categoriesCtx, {
+    type: 'bar',
+    data: {
+        labels: categoriesData.map(a => a.name),
+        datasets: [{
+            label: 'Spending',
+            data: categoriesData.map(a => Math.abs(a.amount / 100)),
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                ticks: {
+                    callback: function (value, index, values) {
+                        return value.toLocaleString("en-GB", { style: "currency", currency: "GBP" });
+                    }
+                }
+            },
+        },
+        plugins: {
+            tooltip: {
+                callbacks: {
+                    label: function (context) {
+                        return context.dataset.label + ': ' + context.parsed.y.toLocaleString("en-GB", { style: "currency", currency: "GBP" });
+                    }
+                }
+            },
+        }
+    }
+});
